b46835350d233b81d0bb0a4b820f6849979cf257
RIcardo Roldan Smirnov <roldansmirnov@gmail.com>
2013-06-25 18:04:32 -0500

    fgdfgdfgdfgdfg

--- src/com/example/pruebagit/MainActivity.java
+++ src/com/example/pruebagit/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		//prueba RUSO
+		//prueba RUSO asasssas
 	}
 
 	@Override
